6fd63acda45490298ce9db105de2bb7349947294
cogumelo <mcalves031@gmail.com>
2022-12-29 19:10:37 -0300

    Classe setando as strings

---
+++ app/src/main/java/com/example/agendavirtual/util/Constants.kt
@@ -0,0 +1,19 @@
+package com.example.agendavirtual.util
+
+class Constants {
+
+    companion object {
+        const val FILL_FIELDS = "Preencha os campos corretamente"
+        const val ADDED = "Contato adicionado com sucesso"
+        const val NO_ADDED = "Contato não adicionado"
+        const val EQUALS = "Contatos iguais"
+        const val EDITED = "Editado com sucesso"
+        const val NO_EDITED = "Falha na atualização"
+        const val DELETED = "Contato deletado com sucesso"
+        const val CONFIRMATION = "Realmente deseja deletar este contato?"
+        const val CONTACT_EXIST = "Esse contato já existe"
+        const val YES = "Sim"
+        const val NO = "Não"
+        const val ATTENTION = "Atenção"
+    }
+}
